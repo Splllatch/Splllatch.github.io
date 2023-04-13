4393c28b34445af1e1968bd1114e27ae1ea12724
Splllatch <88391981+Splllatch@users.noreply.github.com>
2023-04-13 16:04:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -53,3 +53,78 @@ Contact.onclick = () => {
     Galerie.classList.remove('On')
     Contact.classList.add('On')
 }
+
+const ratio = .9	
+var options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.9
+  }
+
+  const AcceuilIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if(entry.intersectionRatio > ratio) {
+            LogoAcceuil.classList.add('On') 
+            LogoRéalisation.classList.remove('On')
+            LogoGalerie.classList.remove('On')
+            LogoContact.classList.remove('On') 
+            Acceuil.classList.add('On')
+            Réalisation.classList.remove('On')
+            Galerie.classList.remove('On')
+            Contact.classList.remove('On')
+        }})  
+}  
+
+
+
+const RéalisationIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if(entry.intersectionRatio > ratio) {
+            LogoAcceuil.classList.remove('On') 
+            LogoRéalisation.classList.add('On')
+            LogoGalerie.classList.remove('On')
+            LogoContact.classList.remove('On')   
+            Acceuil.classList.remove('On')
+            Réalisation.classList.add('On') 
+            Galerie.classList.remove('On')
+            Contact.classList.remove('On')
+        }})  
+}
+
+const GalerieIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if(entry.intersectionRatio > ratio) {
+            LogoAcceuil.classList.remove('On') 
+            LogoRéalisation.classList.remove('On')
+            LogoGalerie.classList.add('On')
+            LogoContact.classList.remove('On')   
+            Acceuil.classList.remove('On')
+            Réalisation.classList.remove('On') 
+            Galerie.classList.add('On')
+            Contact.classList.remove('On')
+        }})  
+}
+
+const ContactIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if(entry.intersectionRatio > ratio) {
+            LogoAcceuil.classList.remove('On') 
+            LogoRéalisation.classList.remove('On')
+            LogoGalerie.classList.remove('On')
+            LogoContact.classList.add('On')   
+            Acceuil.classList.remove('On')
+            Réalisation.classList.remove('On') 
+            Galerie.classList.remove('On')
+            Contact.classList.add('On')
+        }})  
+}
+
+var AcceuilObserver = new IntersectionObserver(AcceuilIntersect, options);
+var RéalisationObserver = new IntersectionObserver(RéalisationIntersect, options);
+var GalerieObserver = new IntersectionObserver(GalerieIntersect, options);
+var ContactObserver = new IntersectionObserver(ContactIntersect, options);
+
+AcceuilObserver.observe(document.querySelector('.Acceuil'))
+RéalisationObserver.observe(document.querySelector('.Réalisation'))
+GalerieObserver.observe(document.querySelector('.Galerie'))
+ContactObserver.observe(document.querySelector('.Contact'))
